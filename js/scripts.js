46017342a6d44a4339f9ce1ae4c6571ce5fb7fc7
rscales02 <rscales02@yahoo.com>
2017-09-08 03:40:52 -0600

    37th time is a charm
    37th time is a charm

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-	alert("Hello World!");
-})
+
+	var quote;
+	var author;
+
+    function getNewQuote() {
+        $.ajax({
+            url: 'https://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp',
+            dataType: 'jsonp',
+            data: {
+                method: 'getQuote',
+                lang: 'en',
+                format: 'jsonp',
+            },
+            success: function(response) {
+            	quote = response.quoteText;
+            	$("#quote").text(quote)
+            	author = response.quoteAuthor;
+            	if (author) {
+            		$("#author").text("-" + author);
+            	}else {
+            		$("#author").text("-Anonymous");
+            	}
+            }
+        });
+    }
+    getNewQuote();
+
+    $('#newQuote').on('click', function(event){
+    	event.preventDefault();
+    	getNewQuote();
+    });
+
+    $('.twitter-share').on('click', function(event) {
+    	event.preventDefault();
+    	window.open('https://www.twitter.com/intent/tweet?text=' + encodeURIComponent(quote + ' -- ' + author));
+    });
+});
